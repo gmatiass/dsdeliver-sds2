3bc6e9c4a06d7f3231d10359620320a4f321257e
mindblows <gmatias1793@gmail.com>
2021-01-24 15:01:17 -0300

    Add routes and StepsHeader

--- front-web/src/Footer/index.tsx
+++ front-web/src/Footer/index.tsx
@@ -1,17 +1,19 @@
 import './styles.css'
-import {ReactComponent as Logo} from './logo.svg';
-import {ReactComponent as YoutubeIcon} from './youtube.svg';
-import {ReactComponent as LinkedinIcon} from './linkedin.svg';
-import {ReactComponent as InstagramIcon} from './instagram.svg';
+import { ReactComponent as GithubIcon } from './github.svg';
+import { ReactComponent as LinkedinIcon } from './linkedin.svg';
+import { ReactComponent as InstagramIcon } from './instagram.svg';
 
-function Footer(){
-    return(
+function Footer() {
+    return (
         <footer className="main-footer">
             App desenvolvido durante a 2ª ed. do evento Semana DevSuperior
             <div className="footer-icons">
-                <a href="https://github.com/mindblows" target="_new"><YoutubeIcon /></a>
+                <a href="https://github.com/mindblows" target="_new"><GithubIcon /></a>
                 <a href="https://www.linkedin.com/in/gabriel-silva-5a58201a2/" target="_new"><LinkedinIcon /></a>
                 <a href="https://www.instagram.com/gmatiass/" target="_new"><InstagramIcon /></a>
+            </div>
+            <div className="footer-assign">
+                Ícones feitos por <a href="https://www.flaticon.com/br/autores/pixel-perfect" title="Pixel perfect">Pixel perfect</a> de <a href="https://www.flaticon.com/br/" title="Flaticon">www.flaticon.com</a>
             </div>
         </footer>
     );
